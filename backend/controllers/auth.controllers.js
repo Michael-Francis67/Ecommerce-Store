4218092a3c6b614c0fa0356c9674e77236a55f95
Onuzulike Ikechukwu Michael <michealikechukwu06@gmail.com>
2025-07-11 12:01:44 +0100

    Forgot about another token

--- backend/controllers/auth.controllers.js
+++ backend/controllers/auth.controllers.js
@@ -33,7 +33,7 @@ const setCookies = (res, accessToken, refreshToken) => {
     res.cookie("refreshToken", refreshToken, {
         httpOnly: true, // Prevents client-side JavaScript from accessing the cookie or XSS attacks
         secure: process.env.NODE_ENV === "production",
-        sameSite: "Strict", // Helps prevent CSRF attacks
+        sameSite: "None", // Helps prevent CSRF attacks
         maxAge: 7 * 24 * 60 * 60 * 1000, // 7 days
     });
 };
