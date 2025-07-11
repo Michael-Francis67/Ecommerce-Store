b47d66625243ddf9adb2c4e2565c3a46b1047ab1
Onuzulike Ikechukwu Michael <michealikechukwu06@gmail.com>
2025-07-11 15:34:37 +0100

    Updated my repo for production

--- backend/middlewares/auth.middleware.js
+++ backend/middlewares/auth.middleware.js
@@ -2,48 +2,47 @@ import jwt from "jsonwebtoken";
 import User from "../models/user.models.js";
 
 export const protectRoute = async (req, res, next) => {
-  try {
-    const token = req.cookies.accessToken || req.headers.authorization?.split(" ")[1];
-    // Check if the token is provided
-
-    if (!token) {
-      return res.status(401).json({ message: "Unauthorized - No access token provided" });
-    }
-
     try {
-      // Verify the token using the secret key
-      const decoded = jwt.verify(token, process.env.ACCESS_TOKEN_SECRET);
+        const token = req.cookies.accessToken;
+        // Check if the token is provided
 
-      const user = await User.findById(decoded.userId).select("-password"); // Exclude password and version from the user object
+        if (!token) {
+            return res.status(401).json({message: "Unauthorized - No access token provided"});
+        }
 
-      // Check if the user exists
-      if (!user) {
-        return res.status(404).json({ message: "User not found" });
-      }
+        try {
+            // Verify the token using the secret key
+            const decoded = jwt.verify(token, process.env.ACCESS_TOKEN_SECRET);
 
-      req.user = user; // Attach the user to the request object
-      next();
+            const user = await User.findById(decoded.userId).select("-password"); // Exclude password and version from the user object
+
+            // Check if the user exists
+            if (!user) {
+                return res.status(404).json({message: "User not found"});
+            }
+
+            req.user = user; // Attach the user to the request object
+            next();
+        } catch (error) {
+            if (error.name === "TokenExpiredError") {
+                throw error; // If the token is expired, throw an error to be caught below
+            }
+        }
     } catch (error) {
-      if (error.name === "TokenExpiredError") {
-        throw error; // If the token is expired, throw an error to be caught below
-      }
+        console.error("Error in protectRoute middleware:", error);
+        if (error.name === "TokenExpiredError") {
+            return res.status(401).json({message: "Unauthorized - Access token expired"});
+        }
+        return res.status(500).json({message: "Internal server error", error: error.message});
     }
-
-  } catch (error) {
-    console.error("Error in protectRoute middleware:", error);
-    if (error.name === "TokenExpiredError") {
-      return res.status(401).json({ message: "Unauthorized - Access token expired" });
-    }
-    return res.status(500).json({ message: "Internal server error", error: error.message });
-  }
 };
 
 export const adminRoute = (req, res, next) => {
-  // Check if the user has admin role
-  if (req.user && req.user.role === "admin") {
-    return next(); // Proceed to the next middleware or route handler
-  } else {
-    // If the user is not an admin, return a 403 Forbidden response
-    return res.status(403).json({ message: "Forbidden - Admin access required" });
-  }
+    // Check if the user has admin role
+    if (req.user && req.user.role === "admin") {
+        return next(); // Proceed to the next middleware or route handler
+    } else {
+        // If the user is not an admin, return a 403 Forbidden response
+        return res.status(403).json({message: "Forbidden - Admin access required"});
+    }
 };
--- backend/server.js
+++ backend/server.js
@@ -41,12 +41,6 @@ app.use("/api/analytics", analyticsRoutes);
 app.use("/api/shipping-address", shippingAddressRoutes);
 app.use("/api/reviews", reviewRoutes);
 
-app.use(express.static(path.join(__dirname, "../frontend/dist")));
-
-app.get("{0,}", (req, res) => {
-    res.sendFile(path.join(__dirname, "frontend", "dist", "index.html"));
-});
-
 // Start the server
 app.listen(PORT, () => {
     console.log(`Server running on port http://localhost:${PORT}`);
