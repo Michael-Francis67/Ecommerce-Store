5ef0dc56b40a87433522997c2e89b99fcd9b7ec7
Onuzulike Ikechukwu Michael <michealikechukwu06@gmail.com>
2025-07-11 13:08:30 +0100

    M

--- frontend/src/lib/axios.js
+++ frontend/src/lib/axios.js
@@ -1,6 +1,6 @@
 import axios from "axios";
 
-const BASE_URL = `${import.meta.env.VITE_BASE_URL}/api`;
+const BASE_URL = "https://ecommerce-store-13.onrender.com/api";
 
 const axiosInstance = axios.create({
     baseURL: BASE_URL, // Use the appropriate base URL for development or production
