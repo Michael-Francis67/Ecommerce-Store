1e7c3bbee15730ab7020c2323a86ad522888860e
Onuzulike Ikechukwu Michael <michealikechukwu06@gmail.com>
2025-07-11 11:39:25 +0100

    One last change

--- backend/controllers/auth.controllers.js
+++ backend/controllers/auth.controllers.js
@@ -98,7 +98,7 @@ export const login = async (req, res) => {
                 },
             });
         } else {
-            res.status(401).json({message: "Invalid email or password"});
+            res.status(400).json({message: "Invalid email or password"});
         }
     } catch (error) {
         console.error("Error during login:", error);
