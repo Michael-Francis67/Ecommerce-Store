9c6e456251289de80c22a6171de0b4b29f063f4a
Onuzulike Ikechukwu Michael <michealikechukwu06@gmail.com>
2025-07-13 18:51:51 +0100

    NNNNN

--- backend/server.js
+++ backend/server.js
@@ -35,7 +35,7 @@ app.use("/api/reviews", reviewRoutes);
 // Serve static files
 app.use(express.static(path.join(__dirname, "../frontend/dist")));
 // Serve the React app
-app.get("/", (req, res) => {
+app.get("(.*)", (req, res) => {
     res.sendFile(path.join(__dirname, "frontend", "dist", "index.html"));
 });
 
